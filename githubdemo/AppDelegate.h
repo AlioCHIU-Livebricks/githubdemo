a3db29c0706fb898b6dfae808399e4e7325c6c1f
AlioCHIU <alio.chiu@livebricks.com>
2014-01-09 18:23:12 +0800

    demo project init

---
+++ githubdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  githubdemo
+//
+//  Created by AlioCHIU on 2014/1/9.
+//  Copyright (c) 2014年 LiVEBRiCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ githubdemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  githubdemo
+//
+//  Created by AlioCHIU on 2014/1/9.
+//  Copyright (c) 2014年 LiVEBRiCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
